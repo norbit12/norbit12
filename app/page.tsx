8cf1ce6724b6657f716dd2d4e54d41bfb5d5e84e
revelsprk <revelsprk@gmail.com>
2026-01-27 18:46:27 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   >("articles");
 
   return (
-    <div className="max-w-md w-full md:w-1/3 mx-4 md:mx-auto my-6">
+    <div className="max-w-md w-full md:w-1/3 px-4 md:px-0 md:mx-auto my-6">
       <Profile />
       <div className="mt-8 grid grid-cols-1 md:grid-cols-3 border border-slate-300 divide-y md:divide-y-0 md:divide-x divide-slate-300">
         <button onClick={() => setActiveTab("articles")} className={`cursor-pointer py-1 hover:bg-slate-100 ${activeTab === "articles" ? "bg-slate-100 font-semibold" : ""}`}>Articles</button>
